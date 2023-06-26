b33208a02940b82712b58f40223328aa388f3764
Lionel Valls <lionelvalls@gmail.com>
2023-06-26 20:09:37 -0300

    arreglo del detalle

--- src/asyncMock.jsx
+++ src/asyncMock.jsx
@@ -84,7 +84,7 @@ export const getProducts= () => {
 export const getProductById = (productId) => {
     return new Promise((resolve) => {
         setTimeout(() => {
-            resolve (products.find(prod => prod.id === productId))
+            resolve (products.find(prod => prod.id === parseInt(productId)))
         },500)
     }) 
 }
@@ -92,7 +92,7 @@ export const getProductById = (productId) => {
 export const getProductByCategory = (productCategory) => {
     return new Promise((resolve) => {
         setTimeout(() => {
-            resolve (products.find(prod => prod.category === productCategory))
+            resolve (products.filter(prod => prod.category === productCategory))
         },500)
     }) 
 }
--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,20 +1,24 @@
-import CartWidget from  '../CartWidget';
 import './navBar.css';
+import { useContext } from "react"
+import CartWidget from  '../CartWidget';
+
 import { NavLink, Link } from 'react-router-dom';
 
 export const NavBar = () => {
     return (
         <header className= "header"> 
-            <div className="header__container">
-                <img src = '/favicon.ico' alt="logo" />
+            <div className="header__container">  
+                <Link to='/'>
+                        <img src = '../../img/logo.png' alt="logo" />
+                    </Link>
                 
                 <nav className="header__nav">
                     <Link to='/'>
                         <h3>Vehiculos</h3>
                     </Link>
                     <div className="categorias">
-                        <NavLink to={`/category/Usados`} className={({isActive}) => isActive ? 'ActiveOption': 'Option'}>Usados</NavLink>
-                        <NavLink to={`/category/Nuevos`} className={({isActive}) => isActive ? 'ActiveOption': 'Option'}>Nuevos</NavLink>
+                        <NavLink to={"/Category/Usados"} className={({isActive}) => isActive ? 'ActiveOption': 'Option'}>Usados</NavLink>
+                        <NavLink to={"/Category/Nuevos"} className={({isActive}) => isActive ? 'ActiveOption': 'Option'}>Nuevos</NavLink>
                     </div>
                     <CartWidget />  
                 </nav>
